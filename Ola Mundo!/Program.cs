00b0fb5eadc30eab52211609a756ebe7ae75fb7e
Tiakowski <tiagosantosrazr@gmail.com>
2022-06-14 14:37:33 -0300

    Ola, mundo

---
+++ Ola Mundo!/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tecle ENTER para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ Variaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int idade;
+        int ano;
+        int nascimento;
+
+        idade = 22;
+        ano = 2022;
+        nascimento = ano - idade;
+
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Ano atual: " + ano);
+        Console.WriteLine("Ano de nascimento: " + nascimento);
+        Console.ReadLine();
+    }
+}
