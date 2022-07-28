a86d1be2150dad3e781ad319f6282764c423e865
Tiakowski <tiagosantosrazr@gmail.com>
2022-07-28 19:32:30 -0300

    Namespace

---
+++ Orientação a Objeto/Banco/Program.cs
@@ -0,0 +1,33 @@
+﻿using Banco;
+
+//Console.WriteLine("Bem vindo ao Banco Banco\n");
+
+//ContaCorrente conta1 = new ContaCorrente();
+//conta1.conta = "132456-F";
+//conta1.numeroAgencia = 9;
+//conta1.nomeAgencia = "Conta Central";
+//conta1.saldo = 100;
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.conta = "124543-F";
+//conta2.numeroAgencia = 14;
+//conta2.nomeAgencia = "Conta Central";
+//conta2.saldo = 100;
+
+
+//Console.WriteLine("Saldo do Gabriel pré-depósito: "+conta1.saldo);
+//Console.WriteLine("Saldo do Arrascaeta pré-depósito: " + conta2.saldo);
+//Console.WriteLine("Fazendo a operação!");
+//conta1.Transferir(50, conta2);
+//Console.WriteLine("Saldo do Gabriel pós-depósito: " + conta1.saldo);
+//Console.WriteLine("Saldo do Arrascaeta pós-depósito: " + conta2.saldo);
+
+Cliente cliente1 = new Cliente();
+cliente1.nome = "Gabriel";
+cliente1.cpf = "028.350.218-50";
+cliente1.profissao = "Atleta";
+
+Console.ReadKey();
+
+
+
---
+++ Orientação a Objeto/Banco/contaCorrente.cs
@@ -0,0 +1,43 @@
+﻿namespace Banco
+{
+    public class ContaCorrente
+    {
+        public string conta;
+        public string nomeAgencia;
+        public  int numeroAgencia;  
+        public double saldo;
+    
+
+    public bool Sacar(double valor)
+    {
+        if(saldo < valor || valor < 0)
+        {
+            return false;
+        } else
+        {
+            saldo = saldo - valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+    public bool Transferir (double valor, ContaCorrente destino)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+} 
